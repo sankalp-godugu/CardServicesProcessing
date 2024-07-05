69616af9d4d5283ef02d81d3f3b90783c0f35dd5
Sankalp Godugu <sankalp.godugu@outlook.com>
2024-07-05 16:44:38 -0400

    Update EmailConstants.cs

--- CardServicesProcessing/Shared/EmailConstants.cs
+++ CardServicesProcessing/Shared/EmailConstants.cs
@@ -9,6 +9,7 @@
         public static readonly string MargaretAnnTapia = "mtapia@nationsbenefits.com";
         public static readonly string VijayanRayan = "vrayan@nationsbenefits.com";
         public static readonly string SankalpGodugu = "sankalp.godugu@nationsbenefits.com";
+        public static readonly string SabriAttalla = "sattalla@nationsbenefits.com";
         public static readonly string AustinStephens = "astephens@nationsbenefits.com";
         public static readonly string AustinStephensTest = "ahstephens01@gmail.com";
         public static readonly string DoNotReply = "donotreply_zendesk@nationsbenefits.com";
